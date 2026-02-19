abafb90d33c3878b30b567e238b5b7533081ee2f
mohahmd_darwish <mohamad@gmail.com>
2026-02-19 15:13:53 +0300

    "التعديل على خصائص المريض "

--- ConsoleApp1/Models/Patient.cs
+++ ConsoleApp1/Models/Patient.cs
@@ -8,9 +8,15 @@ namespace ConsoleApp1.Models
 {
     public class Patient
     {
-        // هنا نضع معلومات المريض (رقم، اسم، تشخيص)
-        public int Patient_ID { get; set; }
-        public string Name { get; set; }
-        public string Diagnosis { get; set; }
+        public int Patient_ID { get; set; } // رقم المريض
+        public string First_Name { get; set; } // الاسم الأول
+        public string Last_Name { get; set; } // الكنية
+        public DateTime Date_of_Birth { get; set; } // تاريخ الميلاد
+        public string Gender { get; set; } // الجنس
+        public string Phone_Number { get; set; } // رقم الهاتف
+        public string Address { get; set; } // العنوان
+        public string Diagnosis { get; set; } // التشخيص
+        public string Admission_Status { get; set; } // حالة الدخول (مثلاً: طوارئ، منوم)
+        public int Assigned_Doctor_ID { get; set; } // رقم الطبيب المسؤول (علاقة الربط)
     }
 }
