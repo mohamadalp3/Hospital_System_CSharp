eaf4b86d5969e063e5f03c99489eafd0088367d8
mohahmd_darwish <mohamad@gmail.com>
2026-02-19 14:55:50 +0300

    "إضافة كلاس المريض"

---
+++ ConsoleApp1/Models/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public class Patient
+    {
+        // هنا نضع معلومات المريض (رقم، اسم، تشخيص)
+        public int Patient_ID { get; set; }
+        public string Name { get; set; }
+        public string Diagnosis { get; set; }
+    }
+}
