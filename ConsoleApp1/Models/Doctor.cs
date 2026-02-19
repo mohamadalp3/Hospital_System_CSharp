ffedc429948c47e29a534cad8088f52a17bdfc48
mohahmd_darwish <mohamad@gmail.com>
2026-02-19 15:02:35 +0300

    "اضافة كلاس  الطبيب "

---
+++ ConsoleApp1/Models/Doctor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    internal class Doctor
+    {
+        public int Doctor_ID { get; set; }
+        public string Name { get; set; }
+        public string Specialty { get; set; } // التخصص
+    }
+}
