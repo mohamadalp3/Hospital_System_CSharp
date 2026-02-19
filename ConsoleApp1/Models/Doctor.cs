c7f3168a63426188c5d1e04ab61faf8c39a4bb4c
mohahmd_darwish <mohamad@gmail.com>
2026-02-19 15:11:41 +0300

    "التعديل على خصائص الطبيب"

--- ConsoleApp1/Models/Doctor.cs
+++ ConsoleApp1/Models/Doctor.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.Models
 {
-    internal class Doctor
-    {
-        public int Doctor_ID { get; set; }
-        public string Name { get; set; }
-        public string Specialty { get; set; } // التخصص
+    
+        // كلاس يمثل بيانات الأطباء بناءً على المخطط المطلوب
+        public class Doctor
+        {
+            public int Doctor_ID { get; set; } // رقم الطبيب (المعرف الفريد)
+            public string First_Name { get; set; } // الاسم الأول
+            public string Last_Name { get; set; } // الكنية
+            public string Specialty { get; set; } // التخصص
+            public string Phone_Number { get; set; } // رقم الهاتف
+            public string Email { get; set; } // البريد الإلكتروني
+            public string Work_Shift { get; set; } // وردية العمل (صباحي/مسائي)
+            public string Department { get; set; } // القسم التابع له
+        }
     }
-}
+
