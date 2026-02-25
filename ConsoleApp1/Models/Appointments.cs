6ce7297ef80ca56001a0517c155dd11e73c08e8c
omarsyrai05-ops <omarsyrai05@gmail.com>
2026-02-25 07:49:48 -0700

    انشاء كلاس للمواعيد

---
+++ ConsoleApp1/Models/Appointments.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    internal class Appointments
+    {
+        
+        public class Appointment
+        {
+            public int AppointmentId { get; set; }       // رقم الموعد
+            public int PatientId { get; set; }           // رقم المريض
+            public int DoctorId { get; set; }            // رقم الطبيب
+            public string ? AppointmentDate { get; set; } // تاريخ ووقت الموعد
+            public string ? Notes { get; set; }            // ملاحظات إضافية
+
+          
+        }
+    }
+
+
+
+}
+
