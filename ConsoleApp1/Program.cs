39b61cc7bfd75874ea333d5f4770ab311d0cf708
mohahmd_darwish <mohamad@gmail.com>
2026-02-26 21:40:27 +0300

    التعديل على بيانات الطبيب

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,7 +16,7 @@ namespace ConsoleApp1
             Doctor doctor1 = new Doctor
             {
                 Doctor_ID = 101,
-                First_Name = "nour",
+                First_Name = "noor",
                 Last_Name = "Doe",
                 Specialty = "Cardiology",
                 Department = "Surgery",
